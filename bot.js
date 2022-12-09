6f40dff3e24e679d7735bd953a68408c239938f6
cgpinta <pinta.christian@outlook.com>
2022-12-09 17:57:15 -0600

    added custom message command

--- bot.js
+++ bot.js
@@ -9,10 +9,13 @@ const client = new Discord.Client({
 		GatewayIntentBits.GuildMembers
 	]
 });
+//permissions integer: 431644736576
+
 const { MessageEmbed } = require('discord.js');
 //const { fs } = require('fs');
 const { token } = require('./config.json');
 const { clientId } = require('./config.json');
+const fs = require('fs');
 
 client.login(token);
 var randomMessage;
@@ -25,7 +28,8 @@ var userIDs = [];
 var readChannel;
 var talkChannel;
 
-var prefix = "MM!";
+var responseMode;
+
 var botName = "matt";
 
 var lowTime = 5;
@@ -51,8 +55,43 @@ async function main() {
 		new SlashCommandBuilder()
 			.setName('setchannels')
 			.setDescription('Sets the channels inwhich Matt will respond and read messages from')
-			.addChannelOption((channel) => channel.setName('talkchannel').setDescription('Sets the channel in which Matt will talk in.').setRequired(true))
-			.addChannelOption((channel) => channel.setName('readchannel').setDescription('Sets the channel in which Matt will read messages from.').setRequired(true)),
+			.addChannelOption((channel) => channel.setName('talkchannel')
+				.setDescription('Sets the channel in which Matt will talk in.')
+				.setRequired(true))
+			.addChannelOption((channel) => channel.setName('readchannel')
+				.setDescription('Sets the channel in which Matt will read messages from.')
+				.setRequired(true)),
+		new SlashCommandBuilder()
+			.setName('setresponsemode')
+			.setDescription('Sets which messages Matt will respond to')
+			.addStringOption((mode) => mode.setName('responsemode')
+				.setDescription('Sets when Matt will respond to a message with his name in it.').setRequired(true)
+				.addChoices(
+					{ name: 'Start', value: 'start' },
+					{ name: 'Start and End', value: 'both' },
+					{ name: 'Contains', value: 'anywhere' },
+				)),
+		new SlashCommandBuilder()
+			.setName('sendmessage')
+			.setDescription('Sends a message as Matt')
+			.addStringOption((str) => str.setName('message')
+				.setDescription('What will Matt say.').setRequired(true)
+				.setRequired(true))
+			.addChannelOption((channel) => channel.setName('channel')
+				.setDescription('Which channel to send the message in.')
+				.setRequired(true)),
+		new SlashCommandBuilder()
+			.setName('sendreply')
+			.setDescription('Sends a reply as Matt')
+			.addStringOption((str) => str.setName('message')
+				.setDescription('What will Matt say.').setRequired(true)
+				.setRequired(true))
+			.addChannelOption((channel) => channel.setName('channel')
+				.setDescription('Which channel is the message in.')
+				.setRequired(true))
+			.addStringOption((id) => id.setName('messageid')
+				.setDescription('ID of message Matt will reply to.').setRequired(true)
+				.setRequired(true)),
 	].map(command => command.toJSON())
 
 
@@ -80,12 +119,12 @@ client.on('messageCreate', (msg) => {
 	var command = msg.content.split(" ");
 
     if (talkChannel != undefined && msg.channel.id == talkChannel.id) {
-        if (msg.content.toLowerCase().startsWith(botName)) {
+        if (isUsableMessage(msg)) {
             randomMsg(msg, true, false);
         }
 	}
 	else if (readChannel != undefined && msg.channel.id == readChannel.id) {
-		if (!msg.content.toLowerCase().startsWith(botName)) {
+		if (isUsableMessage(msg)) {
 			messages.push(msg);
 			console.log("Added \"" + msg.content + "\" to the message list");
         }
@@ -111,8 +150,37 @@ client.on('interactionCreate', interaction => {
 			readChannel = interaction.options.getChannel('readchannel');
 			console.log("talkchannel set to:" + talkChannel.name + " readchannel set to:" + readChannel.name);
 
+			interaction.reply({ content: "scanning through " + readChannel.name, ephemeral: true });
 			fetchAllMessages(readChannel);
-			interaction.reply({ content: "talkchannel set to:" + talkChannel.name + " readchannel set to:" + readChannel.name, ephemeral: true });
+			//interaction.editReply({ content: "talkchannel set to:" + talkChannel.name + " readchannel set to:" + readChannel.name, ephemeral: true });
+			break;
+		case "setresponsemode":
+			if (!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+				interaction.reply({ content: "You can not use this command.", ephemeral: true });
+				return;
+			}
+			const getRespMode = interaction.options.getString('mode');
+			responseMode = getRespMode;
+
+			console.log("Response mode set to:" + responseMode);
+
+			interaction.reply({ content: "Response mode set to:" + responseMode, ephemeral: true });
+			break;
+		case "sendmessage":
+			if (!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+				interaction.reply({ content: "You can not use this command.", ephemeral: true });
+				return;
+			}
+			sendCustomMessage(interaction.options.getString('message'), interaction.options.getChannel('channel'))
+			interaction.reply({ content: "Message sent", ephemeral: true });
+			break;
+		case "sendreply":
+			if (!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+				interaction.reply({ content: "You can not use this command.", ephemeral: true });
+				return;
+			}
+			sendCustomReply(interaction.options.getString('message'), interaction.options.getChannel('channel'), interaction.options.getString('messageId'))
+			interaction.reply({ content: "Replay sent", ephemeral: true });
 			break;
     }
 });
@@ -121,42 +189,40 @@ client.on('interactionCreate', interaction => {
 
 
 async function fetchAllMessages(curChannel) {
-	//acceptingInput = false;
-	//curInteraction.reply({ content: "hold on..." + curInteraction.channel.name, ephemeral: true });
 	console.log("Updating Messages");
 	const channel = client.channels.cache.get(curChannel.id.toString());
 	var loadingTime = 0;
-	//let messages = [];
 
 	// Create message pointer
 	let message = await channel.messages
 		.fetch({ limit: 1 })
 		.then(messagePage => (messagePage.size === 1 ? messagePage.at(0) : null));
 
+	var startTime = Date.now;
 	while (message) {
-		loadingTime++;
-		console.log("	Getting Messages:"+loadingTime);
+		var currentTime = Date.now - startTime;
+		console.log("	Getting Messages:"+messages.length+"		"+currentTime.toString());
 		await channel.messages
 			.fetch({ limit: 100, before: message.id })
 			.then(messagePage => {
-				messagePage.forEach(msg => messages.push(msg));
+				messagePage.forEach(msg => {
+					if (isUsableMessage(msg)) {
+						messages.push(msg);
+						console.log("added: " +msg.author.name+" \""+msg.content+"\"");
+					}
+					else {
+						console.log("skipped: " + msg.content);
+                    }
+				});
 
 				// Update our message pointer to be last message in page of messages
 				message = 0 < messagePage.size ? messagePage.at(messagePage.size - 1) : null;
-			})
+			}
+		)
 	}
-
-	console.log("		Erasing Mattssages");
-	for (let i = 0; i < messages.length; i++) {
-		if (messages[i].content.toLowerCase().startsWith("Matt") || messages[i].author.bot || messages[i].content.startsWith(prefix)) {
-			console.log("UserID:"+messages[i].author.id+", BotID:"+client.user.id+", Deleting:"+messages[i].content);
-			messages.splice(i-1, 1);
-		}
-	}
-	//console.log(messages[100].content);
 	console.log("Messages Updated");
-	//curInteraction.reply({ content: "alright, I'll talk here for now." + interaction.channel.name, ephemeral: true });
-	acceptingInput = true;
+	
+	console.log("Messages saved locally");
 }
 
 function randomIntFromInterval(min, max) {
@@ -191,7 +257,7 @@ function randomMsg(msg, isReply, isRepeating) {
 	console.log("messages length:" + messages.length);
 
 	console.log("ID:" + rand + ", bot? " + messages[rand].author.bot + ", Message: " + messages[rand].content + ", From User: " + messages[rand].author.name);
-	if (!messages[rand].author.bot && !(messages[rand].content.toLowerCase().startsWith(botName)) && !(messages[rand].content.startsWith(prefix)) && messages[rand].content.length > 0) {
+	if (isUsableMessage(messages[rand])) {
 		if (isReply) {
 			console.log("Replying:" + messages[rand].content);
 			msg.reply(messages[rand].content);
@@ -208,6 +274,17 @@ function randomMsg(msg, isReply, isRepeating) {
 	}
 }
 
+function sendCustomMessage(str_message, channel){
+	channel.send(str_message);
+}
+
+function isUsableMessage(message) {
+	if (!message.author.bot && !(message.content.toLowerCase().includes(botName)) && !(message.content.startsWith("/")) && message.content.length > 0) {
+		return true;
+	}
+	return false;
+} 
+
 
 function initializeTime() {
 	timeReset = new Date();
@@ -219,4 +296,20 @@ function doMessagesNeedUpdate() {
 	if (timeNow.getTime() > timeReset.getTime()) {
 		fetchAllMessages();
 	}
+}
+
+function saveArrayToFile(array, filePath, separator) {
+	// convert the array to a string with each item separated by the specified separator character
+	const arrayAsString = array.join(separator);
+
+	// write the string to the file
+	fs.writeFileSync(filePath, arrayAsString);
+}
+
+function loadArrayFromFile(filePath, separator) {
+	// read the file contents as a string
+	const arrayAsString = fs.readFileSync(filePath, 'utf8');
+
+	// split the string into an array by splitting on the specified separator character
+	return arrayAsString.split(separator);
 }
